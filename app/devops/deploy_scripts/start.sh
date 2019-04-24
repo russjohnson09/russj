b3ccbc94694d55051198ce0f9ce7ef02dde97585
Russ Johnson <russj@detroitsoftware.com>
2019-04-24 19:55:54 -0400

    update deploy script

--- app/devops/deploy_scripts/start.sh
+++ app/devops/deploy_scripts/start.sh
@@ -14,15 +14,12 @@ cd node_app
 npm install
 tsc
 pm2 startOrGracefulReload russj.pm2.config.js
-pm2 startOrGracefulReload russj_balanced.pm2.config.js
+pm2 startOrGracefulReload russj_backup.pm2.config.js
 
-#TODO manage these in a more uniformed manner?
-#start microservices
-#echo "start microservices"
-#pm2 startOrGracefulReload microservices.pm2.config.js
-#pm2 startOrGracefulReload microservices_balanced.pm2.config.js
 
-cd /home/ubuntu/node_monolith/node_app2/react
+echo $dir #should be app
+
+cd react_frontend
 
 npm install
 
